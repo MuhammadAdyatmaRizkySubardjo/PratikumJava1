3f25c5be9c8aa130a9633beb218e8e1ad25843d8
unknown <rrifazyu@gmail.com>
2021-06-09 19:01:58 +0700

    deklarasi variable dan tipe data

---
+++ Modul1/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+    public static void main(String[] args){
+
+        String namaDepan ="Muhammad";
+        String namaTengah ="Adyatma Rizky";
+        String namaBelakang = "Subardjo";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89765412;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+    }
+}
