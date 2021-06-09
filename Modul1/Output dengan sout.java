9fe5f031a599113cd88d8568fff08f887f54a5d9
unknown <rrifazyu@gmail.com>
2021-06-09 23:04:20 +0700

    Output dengan sout

---
+++ Modul1/Output dengan sout.java
@@ -0,0 +1,22 @@
+public class DataTypes{
+    public static void main(String[] args){
+
+        String namaDepan ="Muhammad";
+        String namaTengah ="Adyatma Rizky";
+        String namaBelakang = "Subardjo";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89765412;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println("Nama Depan : " + namaDepan);
+        System.out.println("Nama Tengah : " + namaTengah);
+        System.out.println("Nama Belakang :"+ namaBelakang);
+        System.out.print("Usia : " + usia);
+        System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
+        System.out.println("IPK : " + ipk);
+        System.out.println("Nilai PBO " + nilaiAbjad);
+        System.err.println("Tampan :" + tampan );
+    }
+}
