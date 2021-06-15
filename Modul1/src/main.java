c05c79cb5f9942c14600a1b0c2a567a67d4b8445
MuhammadAdyatmaRizkySubardjo <rrifazyu@gmail.com>
2021-06-15 15:17:49 +0700

    Operasi Tipe String

--- Modul1/src/main.java
+++ Modul1/src/main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args){
         String namaDepan = "Muhammad";
         String namaTengah =" Adyatma Rizky ";
-        String namaBelakang = "Kurniawan";
+        String namaBelakang = "Subradjo";
         int usia = 18;
         int targetTahunKuliah = 4;
         double ipk =3.89764512;
@@ -15,5 +15,25 @@ public class main {
 
         char[] uniskaChar = {'U' , 'N', 'I' , 'S' , 'K' , 'A'};
         String uniskaString = new String(uniskaChar);
-        System.out.println(uniskaString); }
+        System.out.println(uniskaString);
+
+
+
+
+    String namaLengkap = namaDepan + " " + namaTengah + " "+ namaBelakang;
+    System.out.println(namaLengkap.length());
+    System.out.println(namaLengkap.indexOf("NIS"));
+    System.out.println(namaLengkap.substring(5));
+    System.out.println(namaLengkap.substring(5,12));
+    System.out.println(namaLengkap.replace("Rizky","Barr"));
+    System.out.println(namaLengkap.toUpperCase());
+    System.out.println(namaLengkap.toLowerCase());
+    System.out.println(namaLengkap.charAt(6));
+
+    String[] namaArray = namaLengkap.split("");
+
+    for(String nama : namaArray){
+    System.out.println(nama);
+    }
+    }
 }
