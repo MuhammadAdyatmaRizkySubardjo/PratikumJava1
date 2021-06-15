0a7bd969551eee7ac59cd63a41d87f4d3a729ae3
MuhammadAdyatmaRizkySubardjo <rrifazyu@gmail.com>
2021-06-15 14:56:14 +0700

    Project IDEA

---
+++ Modul1/src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args){
+        String namaDepan = "Muhammad";
+        String namaTengah =" Adyatma Rizky ";
+        String namaBelakang = "Kurniawan";
+        int usia = 18;
+        int targetTahunKuliah = 4;
+        double ipk =3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        System.out.println(namaDepan + namaTengah + namaBelakang );
+    }
+}
